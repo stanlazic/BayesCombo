07d4dc5ad795447dcf2a460c710c7d6f9d7705f8
brunocontrino <br1contrino@yahoo.co.uk>
2016-07-19 15:44:31 +0100

    update and bsfactor summary added

--- R/plot.BSfactor.R
+++ R/plot.BSfactor.R
@@ -10,17 +10,18 @@
 #'
 #' @return Plot of posterior model probabilities in relation to changes in the prior model probabilities.
 #' @seealso \code{\link{BSfactor}}
-#'
+#' @importFrom utils tail
 #' @export
 #' @examples
-#' x <- BSfactor( beta1 = c(0.068,-0.084,0.175,0.337),
-#'        var1 = c(0.000841,0.002916,0.008649,0.032041),
-#'        beta0 = 0 )
+#' x <- BSfactor( beta1 = c(0.0126474408, 5.0051724138, 1.2975612498, 0.0004762455),
+#'        var1 = c(2.538974e-03, 6.662216e+00, 4.219142e+00, 6.963380e-06),
+#'        beta0 = 0, reverse = TRUE )
 #' plot(x)
 
 plot.BSfactor<- function(x, ... ){
+  namevec<-c("H:0","H:>", "H:<")
   n.prior<- nrow(x$priorMP)
-  plot(x = x$priorMP[,1],y = x$PMP[,1], type = "l", ylim = c(0,1),col = "red", ylab = "Probability", xlab = "H:0 PriorMP",
+  plot(x = x$priorMP[,1],y = x$PMP[,1], type = "l", ylim = c(0,1),col = "red", ylab = "Probability", xlab = paste(namevec[tail(x$priorMP,1)== max(tail(x$priorMP,1))], "prior model probability"),
        main = "Plot of posterior model probabilities vs changing prior model probabilities")
   lines(x$priorMP[,1],x$PMP[,2], col = "blue")
   lines(x$priorMP[,1],x$PMP[,3], col = "green")
---
+++ R/summary.BSfactor.R
@@ -0,0 +1,52 @@
+#' @title Summarize a BSfactor object
+#'
+#' @description Gives summary of various sections of the BSfactor class.
+#'
+#' @details For all section of the BSfactor object which are matrices,
+#'  the summary function will return the first and last rows with the values rounded to 4 decimal points.
+#'  The summary also contains other key pieces of information such as boundary and threshold.
+#'
+#' @param object A object of the class BSfactor
+#' @param ... Additional parameters
+#'
+#' @return A summary of the BSfactor object passed.
+#'
+#' @seealso \code{ \link{BSfactor}}
+#'
+#' @export
+#' @examples
+#' x <- BSfactor( beta1 = c(0.0126474408, 5.0051724138, 1.2975612498, 0.0004762455),
+#'        var1 = c(2.538974e-03, 6.662216e+00, 4.219142e+00, 6.963380e-06),
+#'        beta0 = 0, reverse = TRUE )
+#' summary(x)
+
+
+summary.BSfactor<- function(object, ...){
+  if(class(object) != "BSfactor"){
+    warning("please input object of class BSfactor")
+    stop()
+  }
+  ### PMP
+  n<- length(object$PMP[,1])
+  PMP<- priorMP<- matrix(0, nrow = 2,ncol = 3)
+  PMP[1,]<- round(object$PMP[1,],4)
+  PMP[2,]<- round(object$PMP[n,],4)
+  colnames(PMP)<- colnames(object$PMP)
+
+  ### priorMP
+  priorMP[1,]<- round(object$priorMP[1,],4)
+  priorMP[2,]<- round(object$priorMP[n,],4)
+  colnames(priorMP)<- colnames(object$PMP)
+
+  ### boundary
+  boundary<- round(object$boundary,4)
+
+  ### threshold
+  threshold<- round(object$threshold,4)
+
+  ### hypothesis
+
+  hypothesis <- object$hypothesis
+
+  return(list(PMP = PMP,priorMP = priorMP,boundary = boundary, threshold = threshold, hypothesis = hypothesis))
+}
